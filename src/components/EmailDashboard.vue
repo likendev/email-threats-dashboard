3398eb528d1a0d538a3909a29dab6927ada18893
Liken Tan <likentan@gmail.com>
2023-01-21 22:38:31 +0800

    Part 1
    - Fetch message from server and process accordingly
    - added some styling for the data

--- src/components/EmailDashboard.vue
+++ src/components/EmailDashboard.vue
@@ -1,15 +1,73 @@
 <template>
-  <div>
-
+  <div class="dashboard-container">
+    <div v-for="data in processedData" :key="data.severity">
+      <threats-info
+        :threats-category="data.type"
+        :threats-data="data.data"
+        :severity="data.severity"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import ThreatsInfo from "@/components/ThreatsInfo";
+
 export default {
-  name: "EmailDashboard"
-}
+  name: "EmailDashboard",
+  components: { ThreatsInfo },
+  computed: {
+    processedData() {
+      return this.processData(this.$store.getters.getMessages);
+    },
+  },
+  async created() {
+    await axios
+      .get(
+        "https://abnormalsecurity-public.s3.amazonaws.com/fe_dashboard/adams_keeling/messages.json"
+      )
+      .then((response) => {
+        this.$store.dispatch("updateMessagesData", response.data);
+      });
+  },
+  methods: {
+    processData(messageData) {
+      if (Array.isArray(messageData)) {
+        let spamMessage = messageData.filter((message) => {
+          return message.attackType === "SPAM";
+        });
+
+        let highSeverity = messageData.filter((message) => {
+          return message.attackType !== "SPAM";
+        });
+
+        let messageArr = [];
+        messageArr.push({
+          type: "HIGH_SEVERE",
+          severity: 1,
+          data: highSeverity,
+        });
+        messageArr.push({
+          type: "SPAM",
+          severity: 2,
+          data: spamMessage,
+        });
+
+        return messageArr.sort((a, b) => {
+          a.severity - b.severity;
+        });
+      }
+    },
+  },
+};
 </script>
 
-<style scoped>
-
+<style type="scss" scoped>
+.dashboard-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: stretch;
+  width: 100%;
+}
 </style>
--- src/components/ThreatsInfo.vue
+++ src/components/ThreatsInfo.vue
@@ -1,13 +1,58 @@
 <template>
-
+  <div class="threats-info-card" >
+    <h1 class="threats-count" :class="severityClass">
+      {{ threatsCount }}
+    </h1>
+    <div class="threats-label">
+      {{ threatsCategoryLabel }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ThreatsInfo"
-}
+  name: "ThreatsInfo",
+  props: {
+    threatsCategory: { type: String, required: true },
+    threatsData: { type: Array, required: true },
+    severity: { type: Number, required: true },
+  },
+  computed: {
+    threatsCount() {
+      return this.threatsData.length;
+    },
+    threatsCategoryLabel() {
+      return this.threatsCategory === "SPAM"
+        ? "Spam Messages"
+        : "High Severity Threats";
+    },
+    severityClass() {
+      switch (this.severity) {
+        case 1:
+          return "danger";
+        case 2:
+          return "medium";
+        default:
+          return "";
+      }
+    },
+  },
+};
 </script>
 
-<style scoped>
-
+<style scoped type="scss">
+.threats-info-card {
+  border: solid;
+  border-color: black;
+  border-radius: 5px;
+  text-align: center;
+  padding: 30px;
+  width: 100%;
+}
+.danger {
+  color: red;
+}
+.medium {
+  color: orange;
+}
 </style>
